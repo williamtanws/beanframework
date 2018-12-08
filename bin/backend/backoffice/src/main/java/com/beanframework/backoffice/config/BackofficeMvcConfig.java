470a2bc07e1e3c43c9aa9d7cb76f5356bfafdb32
William Tan <william.tan@bertelsmann.de>
2018-12-08 17:11:52 +0800

    Upgrade to 2.0.x

--- bin/backend/backoffice/src/main/java/com/beanframework/backoffice/config/BackofficeMvcConfig.java
+++ bin/backend/backoffice/src/main/java/com/beanframework/backoffice/config/BackofficeMvcConfig.java
@@ -1,12 +1,18 @@
 package com.beanframework.backoffice.config;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.beanframework.backoffice.interceptor.BackofficeSecurityInterceptor;
+import com.beanframework.user.domain.User;
 
 @Configuration
 public class BackofficeMvcConfig implements WebMvcConfigurer{
@@ -23,6 +29,26 @@ public class BackofficeMvcConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(backofficeSecurityInterceptor()).addPathPatterns(BACKOFFICE_WEBROOT+"/**");
 	}
-
 	
+	@Bean
+	public AuditorAware<String> createAuditorProvider() {
+		return new SecurityAuditor();
+	}
+
+	public class SecurityAuditor implements AuditorAware<String> {
+		@Override
+		public Optional<String> getCurrentAuditor() {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+			if (auth != null) {
+				User user = (User) auth.getPrincipal();
+				if (user.getUuid() == null) {
+					return Optional.empty();
+				}
+				return Optional.of(user.getUuid().toString());
+			} else {
+				return Optional.empty();
+			}
+		}
+	}
 }
