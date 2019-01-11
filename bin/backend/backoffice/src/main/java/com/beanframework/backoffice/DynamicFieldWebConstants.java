3e1f1a99f2a3921822ab07593b5bb66fa88f103f
William Tan <william.tan@bertelsmann.de>
2019-01-11 13:06:50 +0800

    Update

---
+++ bin/backend/backoffice/src/main/java/com/beanframework/backoffice/DynamicFieldWebConstants.java
@@ -0,0 +1,26 @@
+package com.beanframework.backoffice;
+
+public interface DynamicFieldWebConstants {
+
+	public static final String LIST_SIZE = "${module.dynamicfield.list.size}";
+
+	public interface Path {
+
+		public interface Api {
+			public static final String CHECKID = "${path.api.dynamicfield.checkid}";
+		}
+
+		public static final String LANGUAGE = "${path.dynamicfield}";
+	}
+
+	public interface View {
+		public static final String LIST = "${view.dynamicfield.list}";
+	}
+
+	public interface ModelAttribute {
+
+		public static final String CREATE = "dynamicfieldCreate";
+		public static final String UPDATE = "dynamicfieldUpdate";
+		public static final String SEARCH = "dynamicfieldSearch";
+	}
+}
---
+++ bin/backend/backoffice/src/main/java/com/beanframework/backoffice/data/DynamicFieldSearch.java
@@ -0,0 +1,20 @@
+package com.beanframework.backoffice.data;
+
+import com.beanframework.dynamicfield.domain.DynamicField;
+
+public class DynamicFieldSearch extends DynamicField {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 369849270149391977L;
+	private String searchAll;
+
+	public String getSearchAll() {
+		return searchAll;
+	}
+
+	public void setSearchAll(String searchAll) {
+		this.searchAll = searchAll;
+	}
+}
