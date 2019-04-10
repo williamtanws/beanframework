d9b919489e4146193b9556e95add62ff20eca903
William Tan <williamtanws@gmail.com>
2019-04-10 23:31:05 +0800

    Update

--- bin/platform/src/main/java/com/beanframework/platform/web/GlobalExceptionController.java
+++ bin/platform/src/main/java/com/beanframework/platform/web/GlobalExceptionController.java
@@ -1,20 +1,28 @@
-package com.beanframework.platform.web;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-@ControllerAdvice
-public class GlobalExceptionController {
-
-	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	public String handleError405(HttpServletRequest request, Exception e) {
-		ModelAndView mav = new ModelAndView("/405");
-		mav.addObject("exception", e);
-		// mav.addObject("errorcode", "405");
-		return "redirect:" + request.getContextPath();
-	}
-}
+//package com.beanframework.platform.web;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.springframework.ui.Model;
+//import org.springframework.web.HttpRequestMethodNotSupportedException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.NoHandlerFoundException;
+//
+//@ControllerAdvice
+//public class GlobalExceptionController {
+//
+//	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+//	public String handleError405(HttpServletRequest request, Exception e) {
+//		ModelAndView mav = new ModelAndView("/405");
+//		mav.addObject("exception", e);
+//		// mav.addObject("errorcode", "405");
+//		return "redirect:" + request.getContextPath();
+//	}
+//	
+//	@ExceptionHandler(NoHandlerFoundException.class)
+//	public String handleError404(Model model, HttpServletRequest request, Exception e) {
+//		model.addAttribute("error", e.getMessage());
+//		return "error";
+//	}
+//}
