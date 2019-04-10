3b19266fff4d16ea58d15a96cafc46009591f237
William Tan <williamtanws@gmail.com>
2019-04-10 23:42:17 +0800

    Update

--- bin/platform/src/main/java/com/beanframework/platform/web/GlobalErrorController.java
+++ bin/platform/src/main/java/com/beanframework/platform/web/GlobalErrorController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
