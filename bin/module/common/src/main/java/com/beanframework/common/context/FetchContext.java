66e32ccd63518a1c307fe7cf396b7a176d23c808
William Tan <williamtanws@gmail.com>
2019-04-16 14:38:01 +0800

    Update

--- bin/module/common/src/main/java/com/beanframework/common/context/FetchContext.java
+++ bin/module/common/src/main/java/com/beanframework/common/context/FetchContext.java
@@ -9,29 +9,24 @@ import org.springframework.stereotype.Component;
 public class FetchContext {
 
 	public void addFetchProperty(Class<?> modelClass, String property) {
-		if (FetchProperties.getInstance().get(modelClass.getName()) == null) {
+		if (FetchProperties.getInstance().getProperties().get(modelClass.getName()) == null) {
 			Set<String> newProperties = new HashSet<String>();
 			newProperties.add(property);
-			FetchProperties.getInstance().put(modelClass.getName(), newProperties);
+			FetchProperties.getInstance().getProperties().put(modelClass.getName(), newProperties);
 		} else {
-			FetchProperties.getInstance().get(modelClass.getName()).add(property);
+			FetchProperties.getInstance().getProperties().get(modelClass.getName()).add(property);
 		}
 	}
 
 	public boolean isFetchable(Class<?> modelClass, String property) {
-		if (FetchProperties.getInstance().get(modelClass.getName()) != null) {
-			return FetchProperties.getInstance().get(modelClass.getName()).contains(property);
+		if (FetchProperties.getInstance().getProperties().get(modelClass.getName()) != null) {
+			return FetchProperties.getInstance().getProperties().get(modelClass.getName()).contains(property);
 		} else {
 			return false;
 		}
 	}
 
 	public void clearFetchProperties() {
-		FetchProperties.getInstance().clear();
-	}
-
-	public void removeFetchProperty(Class<?> modelClass, String property) {
-		// TODO Auto-generated method stub
-		
+		FetchProperties.getInstance().getProperties().clear();
 	}
 }
