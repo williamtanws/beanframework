76772a4660e19a394dfa6b24adb87a8e493ca965
William Tan <william.tan@bertelsmann.de>
2020-04-05 14:26:12 +0800

    Update

--- bin/module/common/src/main/java/com/beanframework/common/CommonConstants.java
+++ bin/module/common/src/main/java/com/beanframework/common/CommonConstants.java
@@ -5,5 +5,11 @@ public class CommonConstants {
 	public static interface Table {
 		public static final String AUDITOR = "auditor";
 	}
+	
+	public static interface Query {
+		public static final String IS_EMPTY = "IS EMPTY";
+		public static final String IS_NULL = "IS NULL";
+		public static final String ORDER_BY = "ORDER BY";
+	}
 
 }
--- bin/module/common/src/main/java/com/beanframework/common/service/AbstractModelServiceImpl.java
+++ bin/module/common/src/main/java/com/beanframework/common/service/AbstractModelServiceImpl.java
@@ -35,6 +35,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import com.beanframework.common.CommonConstants;
 import com.beanframework.common.context.DtoConverterContext;
 import com.beanframework.common.context.EntityConverterContext;
 import com.beanframework.common.context.InterceptorContext;
@@ -69,7 +70,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	protected boolean STRICT_USE_INTERCEPTOR;
 
 	@Override
-	public void initialDefaultsInterceptor(Collection models, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void initialDefaultsInterceptor(Collection models, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -79,12 +81,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public void initialDefaultsInterceptor(Object model, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void initialDefaultsInterceptor(Object model, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		boolean notIntercepted = true;
 		for (InterceptorMapping interceptorMapping : interceptorMappings) {
 			if (interceptorMapping.getInterceptor() instanceof InitialDefaultsInterceptor) {
-				InitialDefaultsInterceptor<Object> interceptor = (InitialDefaultsInterceptor<Object>) interceptorMapping.getInterceptor();
+				InitialDefaultsInterceptor<Object> interceptor = (InitialDefaultsInterceptor<Object>) interceptorMapping
+						.getInterceptor();
 				if (interceptorMapping.getTypeCode().equals(typeCode)) {
 					interceptor.onInitialDefaults(model, context);
 					notIntercepted = false;
@@ -94,12 +98,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 
 		if (STRICT_USE_INTERCEPTOR) {
 			if (notIntercepted)
-				throw new InterceptorException("Cannot find any load interceptor to intercept target typeCode: " + typeCode);
+				throw new InterceptorException(
+						"Cannot find any load interceptor to intercept target typeCode: " + typeCode);
 		}
 	}
 
 	@Override
-	public void loadInterceptor(Collection models, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void loadInterceptor(Collection models, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -124,12 +130,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 
 		if (STRICT_USE_INTERCEPTOR) {
 			if (notIntercepted)
-				throw new InterceptorException("Cannot find any load interceptor to intercept target typeCode: " + typeCode);
+				throw new InterceptorException(
+						"Cannot find any load interceptor to intercept target typeCode: " + typeCode);
 		}
 	}
 
 	@Override
-	public void prepareInterceptor(Collection models, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void prepareInterceptor(Collection models, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -139,12 +147,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public void prepareInterceptor(Object model, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void prepareInterceptor(Object model, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		boolean notIntercepted = true;
 		for (InterceptorMapping interceptorMapping : interceptorMappings) {
 			if (interceptorMapping.getInterceptor() instanceof PrepareInterceptor) {
-				PrepareInterceptor<Object> interceptor = (PrepareInterceptor<Object>) interceptorMapping.getInterceptor();
+				PrepareInterceptor<Object> interceptor = (PrepareInterceptor<Object>) interceptorMapping
+						.getInterceptor();
 				if (interceptorMapping.getTypeCode().equals(typeCode)) {
 					interceptor.onPrepare(model, context);
 					notIntercepted = false;
@@ -154,12 +164,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 
 		if (STRICT_USE_INTERCEPTOR) {
 			if (notIntercepted)
-				throw new InterceptorException("Cannot find any prepare interceptor to intercept target typeCode: " + typeCode);
+				throw new InterceptorException(
+						"Cannot find any prepare interceptor to intercept target typeCode: " + typeCode);
 		}
 	}
 
 	@Override
-	public void removeInterceptor(Collection models, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void removeInterceptor(Collection models, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -169,7 +181,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public void removeInterceptor(Object model, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void removeInterceptor(Object model, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		boolean notIntercepted = true;
 		for (InterceptorMapping interceptorMapping : interceptorMappings) {
@@ -184,12 +197,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 
 		if (STRICT_USE_INTERCEPTOR) {
 			if (notIntercepted)
-				throw new InterceptorException("Cannot find any remove interceptor to intercept target typeCode: " + typeCode);
+				throw new InterceptorException(
+						"Cannot find any remove interceptor to intercept target typeCode: " + typeCode);
 		}
 	}
 
 	@Override
-	public void validateInterceptor(Collection models, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void validateInterceptor(Collection models, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -199,12 +214,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public void validateInterceptor(Object model, InterceptorContext context, String typeCode) throws InterceptorException {
+	public void validateInterceptor(Object model, InterceptorContext context, String typeCode)
+			throws InterceptorException {
 
 		boolean notIntercepted = true;
 		for (InterceptorMapping interceptorMapping : interceptorMappings) {
 			if (interceptorMapping.getInterceptor() instanceof ValidateInterceptor) {
-				ValidateInterceptor<Object> interceptor = (ValidateInterceptor<Object>) interceptorMapping.getInterceptor();
+				ValidateInterceptor<Object> interceptor = (ValidateInterceptor<Object>) interceptorMapping
+						.getInterceptor();
 				if (interceptorMapping.getTypeCode().equals(typeCode)) {
 					interceptor.onValidate(model, context);
 					notIntercepted = false;
@@ -214,12 +231,14 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 
 		if (notIntercepted) {
 			if (STRICT_USE_INTERCEPTOR)
-				throw new InterceptorException("Cannot find any validate interceptor to intercept target typeCode: " + typeCode);
+				throw new InterceptorException(
+						"Cannot find any validate interceptor to intercept target typeCode: " + typeCode);
 		}
 	}
 
 	@Override
-	public void entityConverter(Collection models, EntityConverterContext context, Class modelClass) throws ConverterException {
+	public void entityConverter(Collection models, EntityConverterContext context, Class modelClass)
+			throws ConverterException {
 
 		Iterator iterator = models.iterator();
 		while (iterator.hasNext()) {
@@ -229,7 +248,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public Object entityConverter(Object model, EntityConverterContext context, String typeCode) throws ConverterException {
+	public Object entityConverter(Object model, EntityConverterContext context, String typeCode)
+			throws ConverterException {
 		for (ConverterMapping interceptorMapping : converterMappings) {
 			if (interceptorMapping.getConverter() instanceof EntityConverter) {
 				EntityConverter interceptor = (EntityConverter) interceptorMapping.getConverter();
@@ -243,7 +263,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	}
 
 	@Override
-	public <T extends Collection> T dtoConverter(Collection models, DtoConverterContext context, String typeCode) throws ConverterException, InterceptorException {
+	public <T extends Collection> T dtoConverter(Collection models, DtoConverterContext context, String typeCode)
+			throws ConverterException, InterceptorException {
 		if (models instanceof List<?>) {
 			List<Object> listModels = new ArrayList<Object>();
 			Iterator iterator = models.iterator();
@@ -264,7 +285,7 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 				DtoConverter interceptor = (DtoConverter) interceptorMapping.getConverter();
 				if (interceptorMapping.getTypeCode().equals(typeCode)) {
 					if (context.getConvertedDtos().contains(((GenericEntity) model).getUuid()) == false) {
-						if(((GenericEntity) model).getUuid() != null) {
+						if (((GenericEntity) model).getUuid() != null) {
 							context.getConvertedDtos().add(((GenericEntity) model).getUuid());
 						}
 						return interceptor.convert(model, context);
@@ -278,14 +299,16 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 		throw new ConverterException("Cannot find any dto convert to convert target typeCode: " + typeCode);
 	}
 
-	protected <T> Page readPage(TypedQuery query, final Class domainClass, Pageable pageable, @Nullable Specification spec) {
+	protected <T> Page readPage(TypedQuery query, final Class domainClass, Pageable pageable,
+			@Nullable Specification spec) {
 
 		if (pageable.isPaged()) {
 			query.setFirstResult((int) pageable.getOffset());
 			query.setMaxResults(pageable.getPageSize());
 		}
 
-		return PageableExecutionUtils.getPage(query.getResultList(), pageable, () -> executeCountQuery(getCountQuery(spec, domainClass)));
+		return PageableExecutionUtils.getPage(query.getResultList(), pageable,
+				() -> executeCountQuery(getCountQuery(spec, domainClass)));
 	}
 
 	protected <T> TypedQuery getCountQuery(@Nullable Specification spec, Class domainClass) {
@@ -368,7 +391,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 		}
 	}
 
-	protected Query createQuery(Map<String, Object> properties, Map<String, Sort.Direction> sorts, String data, Integer firstResult, Integer maxResult, Class modelClass) {
+	protected Query createQuery(Map<String, Object> properties, Map<String, Sort.Direction> sorts, String data,
+			Integer firstResult, Integer maxResult, Class modelClass) {
 		String qlString = "select " + (StringUtils.isBlank(data) ? "o" : data) + " from " + modelClass.getName() + " o";
 
 		if (properties != null && properties.isEmpty() == Boolean.FALSE) {
@@ -381,7 +405,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 		Query query = entityManager.createQuery(qlString);
 		if (properties != null && properties.isEmpty() == Boolean.FALSE) {
 			for (Map.Entry<String, Object> entry : properties.entrySet()) {
-				if (entry.getValue() != null) {
+				if (entry.getValue() != null && entry.getValue() instanceof String == false
+						&& entry.getValue().toString().trim().equalsIgnoreCase("IS EMPTY") == false) {
 					query.setParameter(entry.getKey().replace(".", "_"), entry.getValue());
 				}
 			}
@@ -405,7 +430,10 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 		for (Map.Entry<String, Object> entry : properties.entrySet()) {
 			if (propertiesBuilder.length() == 0) {
 				if (entry.getValue() == null) {
-					propertiesBuilder.append("o." + entry.getKey() + " IS NULL");
+					propertiesBuilder.append("o." + entry.getKey() + " "+CommonConstants.Query.IS_NULL);
+				} else if (entry.getValue() instanceof String
+						&& entry.getValue().toString().trim().equalsIgnoreCase(CommonConstants.Query.IS_EMPTY)) {
+					propertiesBuilder.append("o." + entry.getKey() + " "+CommonConstants.Query.IS_EMPTY);
 				} else {
 					propertiesBuilder.append("o." + entry.getKey() + " = :" + entry.getKey().replace(".", "_"));
 				}
@@ -420,7 +448,8 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 		StringBuilder sortsBuilder = new StringBuilder();
 		for (Entry<String, Direction> entry : sorts.entrySet()) {
 			if (sortsBuilder.length() == 0) {
-				sortsBuilder.append("order by o." + entry.getKey() + " " + entry.getValue().toString());
+				sortsBuilder.append(
+						CommonConstants.Query.ORDER_BY + " o." + entry.getKey() + " " + entry.getValue().toString());
 			} else {
 				sortsBuilder.append(", o." + entry.getKey() + " " + entry.getValue().toString());
 			}
@@ -431,6 +460,7 @@ public abstract class AbstractModelServiceImpl implements ModelService {
 	protected <T> Page<T> page(@Nullable Specification spec, Pageable pageable, Class modelClass) {
 
 		TypedQuery<T> query = getQuery(spec, pageable, modelClass);
-		return pageable.isUnpaged() ? new PageImpl<T>(query.getResultList()) : readPage(query, modelClass, pageable, spec);
+		return pageable.isUnpaged() ? new PageImpl<T>(query.getResultList())
+				: readPage(query, modelClass, pageable, spec);
 	}
 }
