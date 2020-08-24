66844e604bfa1961e7790be86c47f908e7f4efa5
William Tan <williamtanws@gmail.com>
2020-08-24 11:52:26 +0800

    Update

--- bin/module/common/src/main/java/com/beanframework/common/CommonConstants.java
+++ bin/module/common/src/main/java/com/beanframework/common/CommonConstants.java
@@ -9,6 +9,7 @@ public class CommonConstants {
 	public static interface Query {
 		public static final String IS_EMPTY = "IS EMPTY";
 		public static final String IS_NULL = "IS NULL";
+		public static final String LIKE = "%";
 		public static final String ORDER_BY = "ORDER BY";
 	}
 
