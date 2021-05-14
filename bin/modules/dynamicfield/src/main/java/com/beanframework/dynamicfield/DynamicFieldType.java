b4e96785482d3e44395646ef358f8df31aed3e2d
William Tan <william.tan@bertelsmann.de>
2021-05-14 17:18:41 +0800

    update

--- bin/modules/dynamicfield/src/main/java/com/beanframework/dynamicfield/DynamicFieldType.java
+++ bin/modules/dynamicfield/src/main/java/com/beanframework/dynamicfield/DynamicFieldType.java
@@ -2,7 +2,7 @@ package com.beanframework.dynamicfield;
 
 public enum DynamicFieldType {
   INTEGER("Integer"), FLOATING_POINT("Floating Point"), TEXT("Text"), TEXTAREA("Text Area"), EDITOR(
-      "Editor"), SELECT("Select"), DATE("Date"), BOOLEAN("Boolean"), FILE("File"), CUSTOM("Custom");
+      "Editor"), SELECT("Select"), DATE("Date"), BOOLEAN("Boolean");
 
   private String type;
 
